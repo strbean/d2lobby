6cba05e137c023267cc8db4dda18e348071f6f3b
Joseph Thomas <str.bean.joe@gmail.com>
2021-09-27 13:25:04 -0700

    Clarify usage in button text and add clipboard logo. Add info box telling user text was copied. Add github link.

--- d2lobby.js
+++ d2lobby.js
@@ -2,11 +2,14 @@ window.onload = function () {
     const prefixIn = document.getElementById("prefixIn");
     const numberIn = document.getElementById("numberIn");
     const copyBtn = document.getElementById("copyBtn");
+    const copyBtnText = document.getElementById("copyBtnText");
+    const alertBox = document.getElementById("alertBox");
+    const alertBoxText = document.getElementById("alertBoxText");
 
     const getGameName = () => `${prefixIn.value}${numberIn.value}`;
 
     const renderCopyBtn = function() {
-        copyBtn.innerText = getGameName();
+        copyBtnText.innerText = getGameName();
     };
 
     const prefixFocused = function() {
@@ -18,14 +21,24 @@ window.onload = function () {
     const copyBtnPressed = function() {
         navigator.clipboard.writeText(getGameName());
 
+        // animate alert box showing text was copied to clipboard
+        alertBoxText.innerText = getGameName();
+        alertBox.classList.add("show");
+        setTimeout(() => alertBox.classList.remove("show"), 3000);
+
         // handle leading zeroes
         const nextNumber = numberIn.valueAsNumber + 1
         numberIn.value = `${nextNumber}`.padStart(numberIn.value.trim().length, "0")
         renderCopyBtn();
     };
 
+    const alertBoxClicked = function() {
+        alertBox.classList.remove("show")
+    }
+
     prefixIn.onfocus = prefixFocused;
     prefixIn.oninput = renderCopyBtn;
     numberIn.oninput = renderCopyBtn;
     copyBtn.onclick = copyBtnPressed;
+    alertBox.onclick = alertBoxClicked;
 };
